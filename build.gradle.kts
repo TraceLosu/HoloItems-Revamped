e874ac899208f8d2578071efaf41af3fb373b1ff
Derek Lee <derek.evilcornrules@gmail.com>
2022-06-28 16:51:55 -0700

    Update plugin-yml Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `java-library`
-    id("net.minecrell.plugin-yml.bukkit") version "0.5.1" // Generates plugin.yml
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2" // Generates plugin.yml
     id("com.github.johnrengelman.shadow") version "7.1.2" // Shades and relocates dependencies into our plugin jar
     id("xyz.jpenilla.run-paper") version "1.0.6" // Adds runServer and runMojangMappedServer tasks for testing
 }
