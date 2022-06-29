14a8f5991270deff6882ae0e428d2a9cff5c9801
Derek Lee <derek.evilcornrules@gmail.com>
2022-06-28 21:31:20 -0700

    Remove custom damage API

--- src/main/java/com/strangeone101/holoitemsapi/Config.java
+++ src/main/java/com/strangeone101/holoitemsapi/Config.java
@@ -6,56 +6,14 @@ import java.io.IOException;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import com.strangeone101.holoitemsapi.item.CustomItem;
-import com.strangeone101.holoitemsapi.util.CustomDamageSource;
 
 public class Config {
 
-    static FileConfiguration deathMessageConfig;
     static FileConfiguration generalConfig;
-    static File deathMessageConfigFile;
     static File generalConfigFile;
-
-    public static boolean hasDeathMessageConfig() {
-        return deathMessageConfig != null;
-    }
 
     public static boolean hasGeneralConfig() {
         return generalConfig != null;
-    }
-
-    public static boolean addDefaultDeathMessage(CustomDamageSource source) {
-        if (!hasDeathMessageConfig()) {
-            return false;
-        }
-
-        deathMessageConfig.addDefault(source.getName() + ".Player", "{player} was killed by {killer}'s " + source.getName());
-        deathMessageConfig.addDefault(source.getName() + ".Other", "{player} was killed by " + source.getName());
-        deathMessageConfig.addDefault(source.getName() + ".Self", "{player} killed themselves with " + source.getName());
-
-        try {
-            deathMessageConfig.save(deathMessageConfigFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-
-    public static String getDeathMessage(CustomDamageSource source, String type) {
-        if (type == null) type = "Other";
-        String defSelf = "{player} killed themselves with " + source.getName();
-        String defOther = "{player} was killed by " + source.getName();
-        String defPlayer = "{player} was killed by {killer}'s " + source.getName();
-        if (hasGeneralConfig()) {
-            if (type.equalsIgnoreCase("other")) return deathMessageConfig.getString(source.getName() + ".Other", defOther);
-            else if (type.equalsIgnoreCase("self")) return deathMessageConfig.getString(source.getName() + ".Self", defSelf);
-            return deathMessageConfig.getString(source.getName() + ".Player", defPlayer);
-        }
-
-        if (type.equalsIgnoreCase("other")) return defOther;
-        else if (type.equalsIgnoreCase("self")) return defSelf;
-        return defPlayer;
-
     }
 
     public static FileConfiguration getGeneralConfig() {
@@ -80,6 +38,4 @@ public class Config {
     public static boolean addDefaultConfigOption(CustomItem item, String option, Object default_) {
         return addDefaultConfigOption("Items." + item.getInternalName() + "." + option, default_);
     }
-
-
 }
