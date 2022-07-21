b60e847ed8f19acf971393c8ebf411e49a9dc719
Derek Lee <derek.evilcornrules@gmail.com>
2022-07-21 04:09:37 -0700

    Implement TrackedBlock

---
+++ src/main/java/com/strangeone101/holoitemsapi/tracking/TrackedBlock.java
@@ -0,0 +1,40 @@
+package com.strangeone101.holoitemsapi.tracking;
+
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
+public record TrackedBlock(UUID worldKey, int x, int y, int z) {
+
+    public TrackedBlock(Block block) {
+        this(block.getWorld().getUID(), block.getX(), block.getY(), block.getZ());
+    }
+
+    public World world() {
+        return Bukkit.getWorld(worldKey);
+    }
+
+    public long chunkKey() {
+        return (long) (x >> 4) & 0xFFFFFFFFL | ((long) (z >> 4) & 0xFFFFFFFFL) << 32;
+    }
+
+    public Chunk chunk() {
+        return world().getChunkAt(x >> 4, z >> 4);
+    }
+
+    public int blockKey() {
+        return y & 0xFFFF | (x & 0xF) << 16 | (z & 0xF) << 24;
+    }
+
+    public Block block() {
+        return world().getBlockAt(x, y, z);
+    }
+
+    @Override
+    public int hashCode() {
+        return worldKey.hashCode() ^ ((y + z * 31) * 31 + x);
+    }
+}
