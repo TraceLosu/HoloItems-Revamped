16a5231a9bd6d0ad573cc24d3c1d1947b8a3e316
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2024-06-02 01:06:36 -0700

    Fast-define Plow
    Didn't do the "difficult" part yet, just wanted something done in two minutes.

--- src/main/java/xyz/holocons/mc/holoitemsrevamp/enchantment/Plow.java
+++ src/main/java/xyz/holocons/mc/holoitemsrevamp/enchantment/Plow.java
@@ -1,9 +1,55 @@
 package xyz.holocons.mc.holoitemsrevamp.enchantment;
 
+import com.destroystokyo.paper.MaterialTags;
 import com.strangeone101.holoitemsapi.enchantment.CustomEnchantment;
 import com.strangeone101.holoitemsapi.enchantment.EnchantmentAbility;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 import xyz.holocons.mc.holoitemsrevamp.HoloItemsRevamp;
 
-public class Plow {
+public class Plow extends CustomEnchantment implements EnchantmentAbility{
 
+    public Plow(HoloItemsRevamp plugin){
+        super(plugin, "plow");
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    @Override
+    public boolean conflictsWith(@NotNull Enchantment enchantment) {
+        return false;
+    }
+
+    @Override
+    public boolean canEnchantItem(@NotNull ItemStack itemStack) {
+        return MaterialTags.SHOVELS.isTagged(itemStack);
+    }
+
+    @Override
+    public @NotNull Component displayName(int i) {
+        return Component.text()
+            .color(NamedTextColor.GRAY)
+            .decoration(TextDecoration.ITALIC, false)
+            .append(Component.text("Plow"))
+            .build();
+    }
+
+    @Override
+    public int getCostMultiplier() {
+        // Copied from Magnet
+        return 12;
+    }
+
+    @Override
+    public void onBlockBreak(BlockBreakEvent event, ItemStack itemStack) {
+        // TODO
+    }
 }
