256d1cc9237d6e938569087720a0020f5ac329d7
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2024-07-20 11:00:30 -0700

    Make getDestination helper function

--- src/main/java/xyz/holocons/mc/holoitemsrevamp/item/LaunchPadBlock.java
+++ src/main/java/xyz/holocons/mc/holoitemsrevamp/item/LaunchPadBlock.java
@@ -5,9 +5,15 @@ import com.strangeone101.holoitemsapi.item.CustomItem;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.Container;
 import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapedRecipe;
+import org.jetbrains.annotations.Nullable;
 import xyz.holocons.mc.holoitemsrevamp.HoloItemsRevamp;
 
 import java.util.List;
@@ -39,4 +45,59 @@ public class LaunchPadBlock extends CustomItem implements BlockAbility {
     }
 
     // TODO: The actual functionality?
+
+    /**
+     * Scans a block to determine the destination location
+     * @param block A block (likely package) to scan
+     * @return The destination location for this package, or null if a destination could not be determined.
+     */
+    // TODO: Remove the SuppressWarnings thing after we actually properly get the destination string
+    @SuppressWarnings({"ConstantValue", "DataFlowIssue"})
+    private @Nullable Location getDestination(Block block) {
+        String destinationString = "0 0";
+        // TODO: Get the destination string.
+
+        // String format: "x_coord z_coord optional world name potentially with spaces"
+        int firstSpace = destinationString.indexOf(' ');
+        if(firstSpace == -1) {
+            // Either only an x coordinate, or no delimeter between X and Z coordinates
+            return null;
+        }
+        int secondSpace = destinationString.indexOf(' ');
+
+        int destinationX;
+        int destinationZ;
+        try {
+            destinationX = Integer.parseInt(destinationString.substring(0, firstSpace));
+            String destinationZString;
+            if(secondSpace == -1) {
+                destinationZString = destinationString.substring(firstSpace+1);
+            }
+            else {
+                destinationZString = destinationString.substring(firstSpace+1, secondSpace);
+            }
+            destinationZ = Integer.parseInt(destinationZString);
+        } catch (NumberFormatException nfe) {
+            // Couldn't get a destination X and Z.
+            return null;
+        }
+
+        World destinationWorld;
+        if(secondSpace == -1) {
+            // In this case, there is no world name, so use the default one.
+            destinationWorld = block.getWorld();
+        }
+        else {
+            // In this case, there is a world name to use.
+            String destinationWorldName = destinationString.substring(secondSpace+1);
+            if(destinationWorldName.isBlank()) {
+                destinationWorld = block.getWorld();
+            }
+            else {
+                destinationWorld = Bukkit.getWorld(destinationWorldName);
+            }
+        }
+
+        return new Location(destinationWorld, destinationX, 352, destinationZ);
+    }
 }
